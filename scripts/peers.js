eac69065f89334356ad7e9e2c90a8d3593c9e6c1
Samad Sajanlal <samad.sajanlal@gmail.com>
2018-09-25 00:23:50 -0500

    replace with working geoip provider

--- scripts/peers.js
+++ scripts/peers.js
@@ -32,7 +32,7 @@ mongoose.connect(dbString, function(err) {
             // peer already exists
             loop.next();
           } else {
-            request({uri: 'http://freegeoip.net/json/' + address, json: true}, function (error, response, geo) {
+            request({uri: 'https://1stbitco.in/api/?ip=' + address, json: true}, function (error, response, geo) {
               db.create_peer({
                 address: address,
                 protocol: body[i].version,
